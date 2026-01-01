06f75b3a5fcd6f6ea28451afd5119fb632eb241b
Sarang-Bangade <bangadesarang@gmail.com>
2026-01-01 23:02:21 +0530

    picture updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,5 @@
 export default function Main() {
 
-  
   return (
     <div className="bg-black min-h-screen w-full flex flex-col text-[16px]">
       <div className="max-w-5xl w-full mx-auto px-10 sm:px-8 md:px-16 py-16 flex flex-col">
@@ -8,7 +7,7 @@ export default function Main() {
         {/* Header */}
         <div className="flex flex-col md:flex-row items-center md:items-center justify-between gap-6">
           <img
-            src="me.png"
+            src="professional_sarang.jpeg"
             alt="Sarang Bangade"
             className="aspect-square h-28 rounded-full border border-neutral-700 object-cover"
           />
